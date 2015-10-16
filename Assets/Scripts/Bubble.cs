5a82eee297490ee1d61c83582479659a4cb292f7
Haley <hne3@github.com>
2015-10-15 23:47:29 -0400

    Bubble will pop if you press G repeatedly

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bubble : MonoBehaviour {
+    
+    private float capacity = 3.0f; // Largest bubble can be before it pops
+    private float inflation = 1.0f; // Current scale
+    private float step = 0.2f; // Amount to increment by
+	
+    void Start()
+    {
+        transform.localScale *= inflation;
+    }
+
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            Grow();
+        }
+	}
+
+    public void Grow()
+    {
+        inflation += step;
+        transform.localScale *= inflation;
+        if(inflation >= capacity)
+        {
+            Pop();
+        }
+    }
+
+    public void Pop()
+    {
+        Destroy(this.gameObject);
+    }
+}
