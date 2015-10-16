4458a7ff6a76232a985bc525449887da99534ea5
Haley <hne3@github.com>
2015-10-16 14:28:32 -0400

    Fancy

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -3,34 +3,42 @@ using System.Collections;
 
 public class Bubble : MonoBehaviour {
     
-    private float capacity = 3.0f; // Largest bubble can be before it pops
-    private float inflation = 1.0f; // Current scale
-    private float step = 0.2f; // Amount to increment by
+    public float capacity = 3.0f; // Largest bubble can be before it pops
+    public float minSize = 1.0f; // Current scale
+    public float inflation = 1.0f; // Current scale
+    public float step = 1.0f; // Amount to increment by
+    private float xRot;
+    private float yRot;
+    private float zRot;
 	
     void Start()
     {
         transform.localScale *= inflation;
-    }
 
-	void Update () {
-        if(Input.GetKeyDown(KeyCode.G))
-        {
-            Grow();
-        }
-	}
+        xRot = Random.Range(-2.0f, 2.0f);
+        yRot = Random.Range(-2.0f, 2.0f);
+        zRot = Random.Range(-2.0f, 2.0f);
+
+        Debug.Log(xRot);
+        Debug.Log(yRot);
+        Debug.Log(zRot);
+    }
 
     public void Grow()
     {
-        inflation += step;
-        transform.localScale *= inflation;
+        transform.localScale.Set(transform.localScale.x + step, transform.localScale.y + step, transform.localScale.z + step);
         if(inflation >= capacity)
         {
-            Pop();
+            step = -step;
+        }
+        if(inflation <= minSize)
+        {
+            step = +step;
         }
     }
 
-    public void Pop()
+    public void Rotate()
     {
-        Destroy(this.gameObject);
+        transform.Rotate(xRot, yRot, zRot);
     }
 }
---
+++ Assets/Scripts/BubbleManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleManager : MonoBehaviour
+{
+
+    public Bubble[] bubbles;
+
+    void Update()
+    {
+        for(int i = 0; i < bubbles.Length; i++)
+        {
+            bubbles[i].Grow();
+            bubbles[i].Rotate();
+        }
+    }
+}
